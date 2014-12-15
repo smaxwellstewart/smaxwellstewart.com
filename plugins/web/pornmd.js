702ad4013ccc03d9d601bf79a846b1d1198b43cc
Simon Maxwell-Stewart <smaxwellstewart@gmail.com>
2014-12-15 01:17:21 -0500

    removed fap searches

--- plugins/web/pornmd.js
+++ plugins/web/pornmd.js
@@ -5,19 +5,7 @@ exports.register = function (plugin, options, next) {
         method: 'GET',
         path: '/labs/fap-searches',
         handler: function (request, reply) {
-            if(request.query.keyword === '') {
-                delete request.query.keyword;
-            }
-            var queryString = qs.stringify(request.query);
-
-            plugin.servers[0].inject('http://localhost.com/api/search/agg?'+queryString, function(res){
-                var data = JSON.parse(res.payload);
-                return reply.view('fap', {
-                 title: 'Fap Searches',
-                    query: request.query,
-                    data: data
-                });
-            });
+            return reply.redirect('http://www.fapsearches.info');
 
             
         }
